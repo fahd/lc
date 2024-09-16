6c0a49b0750f05f6fca46afce251c8b215fa67e9
fahd <asadmansa@icloud.com>
2024-09-16 18:17:37 -0400

    #110 - balanced binary tree

--- py/235 - lowestCommonAncestor.py
+++ py/235 - lowestCommonAncestor.py
@@ -5,28 +5,15 @@
 #         self.left = None
 #         self.right = None
 
-def lca(root:'TreeNode', p:'TreeNode', q:'TreeNode'):
-    if root == None:
-        return
-    l = p if p.val < q.val else q
-    r = p if p.val > q.val else q
-
-    if l.val < root.val and r.val > root.val:
-        return root
-    
-    elif l.val <= root.val and r.val > root.val:
-        return l
-    elif r.val >= root.val and l.val < root.val:
-        return r
-    elif l.val < root.val and r.val < root.val:
-        return lca(root.left, p, q)
-    elif l.val > root.val and r.val > root.val:
-        return lca(root.right, p, q)
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        node = lca(root, p, q)
-        return node
+        if root == None:
+            return None
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
 
 
         
