bae92adaa051f361062c49480d613f4e53a65756
fahd <asadmansa@icloud.com>
2024-09-17 17:46:14 -0400

    278 is_bad_version.py

--- py/278 - is_bad_version.py
+++ py/278 - is_bad_version.py
@@ -1,34 +1,11 @@
-# The isBadVersion API is already defined for you.
-# def isBadVersion(version: int) -> bool:
-
-import math
-
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        s = 1
-        e = n
+        s, e = 1, n
 
         while s <= e:
-            m = math.floor((s + e) / 2)
-            is_bad = isBadVersion(m)
-
-            if isBadVersion(())
-
-            print('m',m)
-            print('is_bad', is_bad)
-
-            if is_bad:
-                if bad_idx < 0:
-                    bad_idx = m
-                else:
-                    bad_idx = min(bad_idx, m)
+            m = (s + e) // 2
+            if isBadVersion(m):
                 e = m - 1
             else:
                 s = m + 1
-            
-        return bad_idx
-                
-            
-
-            
-        
+        return s
