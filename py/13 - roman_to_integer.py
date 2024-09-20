799e8022b32a259a38c70bc258e111c789cb13cf
fahd <asadmansa@icloud.com>
2024-09-20 18:24:27 -0400

    #13 roman_to_integer

--- py/13 - roman_to_integer.py
+++ py/13 - roman_to_integer.py
@@ -11,17 +11,12 @@ class Solution:
         }
         i = len(s) - 2
         c = o[s[-1]]
-
         while i > -1:
-            v = s[i]
-            prev = s[i+1]
-            if v == 'I' and (prev == 'V' or prev == 'X'):
-                c -= 1
-            elif v == 'X' and (prev == 'L' or prev == 'C'):
-                c -= 10
-            elif v == 'C' and (prev == 'D' or prev == 'M'):
-                c -= 100
+            cV = o[s[i]]
+            nV = o[s[i+1]]
+            if cV < nV:
+                c -= cV
             else:
-                c += o[v]
-            i-=1
+                c += cV
+            i -= 1
         return c
