20020c23df27dee8fe6c392bad06eaa38fafa4b2
Frank Sheikh <fahdsheikh@my.unthsc.edu>
2024-10-30 19:57:04 -0400

    start over. added 392

--- py/20 - validParens.py
+++
@@ -1,9 +0,0 @@
-def is_valid(s: str) -> bool:
-    choices = {"{": "}", "[":"]", "(":")"}
-    stack = []
-    for char in s:
-        if stack and stack[-1] in choices and choices[stack[-1]] == char:
-            stack.pop()
-        else:
-            stack.append(char)
-    return not stack
